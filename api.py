f12430817338563763c842c8e7d620efcb30f55e
GEN <93887085+njuforetia@users.noreply.github.com>
2022-11-17 10:32:37 -0700

    Update api.py

--- api.py
+++ api.py
@@ -6,15 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return({"msg": "Welcome home george! Go to '/posts' endpoint for the actual text result"})
-
-@app.route('/posts')
 def george_example():
        return({
-                "author": "George Njuacha", 
-                "project": "Liatrio", 
-                "time": datetime.datetime.now(),
                 "timestamp": time.time(), 
                 "message": "Automate all the things!"
             })
